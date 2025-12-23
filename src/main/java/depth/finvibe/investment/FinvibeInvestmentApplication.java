2323abb1df378507ef78be904766f61bbc3b9d0c
천명현 <cmh1448@naver.com>
2025-12-23 17:03:16 +0900

    Init

---
+++ src/main/java/depth/finvibe/investment/FinvibeInvestmentApplication.java
@@ -0,0 +1,13 @@
+package depth.finvibe.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinvibeInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinvibeInvestmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/depth/finvibe/investment/FinvibeInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package depth.finvibe.investment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinvibeInvestmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
