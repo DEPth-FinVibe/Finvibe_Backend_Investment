d3a312a679ae851b3d61f613f3664ebd68c30e9d
천명현 <cmh1448@naver.com>
2025-12-28 02:37:43 +0900

    feat: 도메인 관련 클래스 구현

---
+++ src/main/java/depth/finvibe/investment/modules/wallet/domain/error/WalletErrorCode.java
@@ -0,0 +1,23 @@
+package depth.finvibe.investment.modules.wallet.domain.error;
+
+import depth.finvibe.investment.shared.error.DomainErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 지갑(Wallet) 모듈 내에서 발생할 수 있는 비즈니스 에러 정의입니다.
+ * 
+ * <p>
+ * {@link DomainErrorCode}를 구현하여 지갑 도메인의 고유한 에러 상황을 명세합니다.
+ * 각 상수는 에러 식별 코드와 다국어 메시지 키를 포함합니다.
+ * </p>
+ */
+@AllArgsConstructor
+@Getter
+public enum WalletErrorCode implements DomainErrorCode {
+  INSUFFICIENT_BALANCE("WALLET_INSUFFICIENT_BALANCE", "error.wallet.insufficient_balance"),
+  INVALID_MONEY_AMOUNT("WALLET_INVALID_MONEY_AMOUNT", "error.wallet.invalid_money_amount");
+
+  private final String code;
+  private final String messageKey;
+}
