23a1bb80effb98732d0e79762230503b47137f2f
taek <wontaek88@gmail.com>
2026-01-03 20:25:27 +0900

    feat: trade 도메인 DTO 구현

---
+++ src/main/java/depth/finvibe/investment/modules/trade/dto/TradeDto.java
@@ -0,0 +1,53 @@
+package depth.finvibe.investment.modules.trade.dto;
+
+import depth.finvibe.investment.modules.trade.domain.enums.MarketType;
+import depth.finvibe.investment.modules.trade.domain.enums.TradeType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+public class TradeDto {
+
+    @Getter
+    @NoArgsConstructor
+    public static class TransactionRequest {
+        private MarketType marketType;
+        private Long stockId;
+        private Double amount;
+        private Long portfolioId;
+        private UUID userId;
+        private TradeType tradeType;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class TradeResponse {
+        private Long tradeId;
+        private MarketType marketType;
+        private Long stockId;
+        private Double amount;
+        private Long price;
+        private Long portfolioId;
+        private UUID userId;
+        private TradeType tradeType;
+
+        public static TradeResponse from(Long tradeId, MarketType marketType, Long stockId, Double amount,
+                                         Long price, Long portfolioId, UUID userId, TradeType tradeType) {
+            return TradeResponse.builder()
+                    .tradeId(tradeId)
+                    .marketType(marketType)
+                    .stockId(stockId)
+                    .amount(amount)
+                    .price(price)
+                    .portfolioId(portfolioId)
+                    .userId(userId)
+                    .tradeType(tradeType)
+                    .build();
+        }
+    }
+}
