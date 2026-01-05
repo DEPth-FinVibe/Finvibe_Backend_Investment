44ccabd044f11a7a64f6c7f0b6f9643a92b24d29
taek <wontaek88@gmail.com>
2026-01-06 01:20:24 +0900

    feat: TradeEventProducer 메소드 구현체 틀 구현

--- src/main/java/depth/finvibe/investment/modules/trade/infra/persistence/TradeKafkaProducer.java
+++ src/main/java/depth/finvibe/investment/modules/trade/infra/persistence/TradeKafkaProducer.java
@@ -1,4 +1,21 @@
 package depth.finvibe.investment.modules.trade.infra.persistence;
 
-public class TradeKafkaProducer {
+import depth.finvibe.investment.modules.trade.application.port.out.TradeEventProducer;
+import depth.finvibe.investment.modules.trade.domain.Trade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TradeKafkaProducer implements TradeEventProducer {
+
+    @Override
+    public void publishNormalTradeExecutedEvent(Trade trade) {
+
+    }
+
+    @Override
+    public void publishReservedTradeExecutedEvent(Trade trade) {
+
+    }
 }
