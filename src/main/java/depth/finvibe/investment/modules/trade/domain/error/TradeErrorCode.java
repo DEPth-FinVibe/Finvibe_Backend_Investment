9bb2cbe5ff0244f29273c4e6155ec7a3c9003d11
taek <wontaek88@gmail.com>
2026-01-06 01:06:48 +0900

    fix: 병합 오류 수정

--- src/main/java/depth/finvibe/investment/modules/trade/domain/error/TradeErrorCode.java
+++ src/main/java/depth/finvibe/investment/modules/trade/domain/error/TradeErrorCode.java
@@ -12,7 +12,7 @@ public enum TradeErrorCode implements DomainErrorCode {
     TRADE_NOT_FOUND("TRADE_NOT_FOUND", "trade.error.tradeNotFound"),
     RESERVED_TRADE_ONLY_CANCELLABLE("RESERVED_TRADE_ONLY_CANCELLABLE", "trade.error.reservedTradeOnlyCancellable"),
     INVALID_TRADE_TYPE("INVALID_TRADE_TYPE", "trade.error.invalidTradeType"),
-    INVALID_TRADE_ID_FORMAT("INVALID_TRADE_ID_FORMAT", "trade.error.invalidTradeIdFormat")
+    INVALID_TRADE_ID_FORMAT("INVALID_TRADE_ID_FORMAT", "trade.error.invalidTradeIdFormat"),
     CANNOT_CANCEL_NON_RESERVED_TRADE("CANNOT_CANCEL_NON_RESERVED_TRADE", "trade.error.cannotCancelNonReservedTrade")
     ;
 
