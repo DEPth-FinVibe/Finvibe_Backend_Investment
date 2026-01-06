d6796bc72af5e750429d97fe2371dab4abac069e
천명현 <cmh1448@naver.com>
2026-01-07 00:26:31 +0900

    feat: KafkaSerializer를 JacksonJsonSerializer로 변경 및 테스트 코드 수정

--- src/test/java/depth/finvibe/investment/FinvibeInvestmentApplicationTests.java
+++ src/test/java/depth/finvibe/investment/FinvibeInvestmentApplicationTests.java
@@ -1,25 +1,17 @@
 package depth.finvibe.investment;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class FinvibeInvestmentApplicationTests {
 
+    @MockitoBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
     @Test
     void contextLoads() {
-    }
-
-    @TestConfiguration
-    static class TestKafkaConfig {
-        @Bean
-        @SuppressWarnings("unchecked")
-        KafkaTemplate<String, Object> kafkaTemplate() {
-            return Mockito.mock(KafkaTemplate.class);
-        }
     }
 }
