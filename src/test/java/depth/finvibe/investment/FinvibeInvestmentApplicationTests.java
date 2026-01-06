9814a4627740bfac11798a46e030fe8926fb99ff
천명현 <cmh1448@naver.com>
2026-01-06 21:44:25 +0900

    feat: KafkaTemplate 모킹을 위한 테스트 구성 추가

--- src/test/java/depth/finvibe/investment/FinvibeInvestmentApplicationTests.java
+++ src/test/java/depth/finvibe/investment/FinvibeInvestmentApplicationTests.java
@@ -1,7 +1,11 @@
 package depth.finvibe.investment;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class FinvibeInvestmentApplicationTests {
@@ -10,4 +14,12 @@ class FinvibeInvestmentApplicationTests {
     void contextLoads() {
     }
 
+    @TestConfiguration
+    static class TestKafkaConfig {
+        @Bean
+        @SuppressWarnings("unchecked")
+        KafkaTemplate<String, Object> kafkaTemplate() {
+            return Mockito.mock(KafkaTemplate.class);
+        }
+    }
 }
