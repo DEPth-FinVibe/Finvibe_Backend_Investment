0586cb0d7a996dbea9d54e182024eb2c4770cc7e
taek <wontaek88@gmail.com>
2026-01-09 11:48:29 +0900

    feat: repository 기본 구현체 클래스 생성

---
+++ src/main/java/depth/finvibe/investment/modules/market/infra/persistence/CurrentPriceRepositoryImpl.java
@@ -0,0 +1,54 @@
+package depth.finvibe.investment.modules.market.infra.persistence;
+
+import depth.finvibe.investment.modules.market.application.port.out.CurrentPriceRepository;
+import depth.finvibe.investment.modules.market.domain.CurrentPrice;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CurrentPriceRepositoryImpl implements CurrentPriceRepository {
+    @Override
+    public void save(CurrentPrice currentPrice) {
+
+    }
+
+    @Override
+    public void saveAll(List<CurrentPrice> currentPrices) {
+
+    }
+
+    @Override
+    public Optional<CurrentPrice> findByStockId(Long stockId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<CurrentPrice> findByStockIds(List<Long> stockIds) {
+        return List.of();
+    }
+
+    @Override
+    public void deleteByStockId(Long stockId) {
+
+    }
+
+    @Override
+    public List<Long> getTopStockIdsByValue(int limit) {
+        return List.of();
+    }
+
+    @Override
+    public List<Long> getTopStockIdsByVolume(int limit) {
+        return List.of();
+    }
+
+    @Override
+    public List<Long> getTopRisingStockIds(int limit) {
+        return List.of();
+    }
+
+    @Override
+    public List<Long> getTopFallingStockIds(int limit) {
+        return List.of();
+    }
+}
---
+++ src/main/java/depth/finvibe/investment/modules/market/infra/persistence/StockRepositoryImpl.java
@@ -0,0 +1,28 @@
+package depth.finvibe.investment.modules.market.infra.persistence;
+
+import depth.finvibe.investment.modules.market.application.port.out.StockRepository;
+import depth.finvibe.investment.modules.market.domain.Stock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class StockRepositoryImpl implements StockRepository {
+    @Override
+    public Page<Stock> findTop100ByOrderByCurrentValueDesc(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Stock> findTop100ByOrderByCurrentVolumeDesc(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Stock> findTop100ByOrderByPrevDayChangePctDesc(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Stock> findTop100ByOrderByPrevDayChangePctAsc(Pageable pageable) {
+        return null;
+    }
+}
