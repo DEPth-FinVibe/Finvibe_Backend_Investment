6f0af439f89002fcc90bbc9b265014ec4daeb795
천명현 <cmh1448@naver.com>
2026-01-13 22:33:02 +0900

    chore: 코멘트 추가

--- src/main/java/depth/finvibe/investment/modules/market/application/port/in/MarketCommandUseCase.java
+++ src/main/java/depth/finvibe/investment/modules/market/application/port/in/MarketCommandUseCase.java
@@ -17,5 +17,9 @@ public interface MarketCommandUseCase {
 
     void removeRegionOfInterestLevel2(List<Long> ownedStockIds);
 
+    // -> 실시간 주가 데이터 말고 과거 주가데이터 조회 정책이 없음 (캐싱정책)
+    // -> 누군가가 어떤 stockId에 대해 (2025-01-01 12:00부터 20개의 분봉 데이터) 조회를 요청함
+    // -> 이중에서 가지고있는데이터와 없는데이터를 파악
+    // -> 가지고있는건 그냥 주고(cache hit), 없으면 외부 API를 이용해 조회후 DB에 저장하고 주고 (cache miss) (write through)\
 
 }
