0cb38f7e94a814369996ba38b838c7125f12a3a5
천명현 <cmh1448@naver.com>
2026-02-09 16:54:19 +0900

    feat(application): JPA 감사 기능 설정 추가
    - JPA 감사 기능을 활성화하기 위한 설정 클래스 추가
    - 기존 애플리케이션 클래스에서 JPA 감사 활성화 주석 제거

---
+++ src/main/java/depth/finvibe/investment/boot/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package depth.finvibe.investment.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
